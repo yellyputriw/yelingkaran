10121e76b898fc6b8017dd6843902e763c906c55
Yelly Putri <yellyputriw@gmail.com>
2022-02-14 18:44:43 +0700

    build struktur with vanilla css and vanilla js

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 const form = document.querySelector('form');
 const content = document.querySelector('.content');
+const piLingkaran = document.querySelector('.pi');
+const innerRadius = document.querySelector('.r');
 
 form.addEventListener('submit', (event) => {
-  const r = document.getElementById('r').value;
+  const r = document.getElementById('radius').value;
   event.preventDefault();
 
-  const hitungLingkaran = (r, phi = r % 7 === 0 ? 22 / 7 : 3.14) => {
-    let luasLingkaran = phi * r ** 2;
+  const hitungLingkaran = (r, pi = r % 7 === 0 ? 22 / 7 : 3.14) => {
+    let luasLingkaran = pi * r ** 2;
     return luasLingkaran;
   };
 
+  const determinePi = () => {
+    if (r % 7 === 0) {
+      piLingkaran.innerHTML = '<sup>22</sup>&frasl;<sub>7</sub>';
+    } else {
+      piLingkaran.innerText = '3.14';
+    }
+  };
+
+  innerRadius.innerText = r;
+
+  determinePi();
   content.innerText = hitungLingkaran(r);
 });
